9496150064df7b7816cf52450ed08106c01ccaef
miketoreno88 <toreno.m@mail.ru>
2022-12-22 20:14:13 +0300

    solution task_3.py solution task_4.py

--- task_4.py
+++ task_4.py
@@ -5,7 +5,23 @@ from pyrob.api import *
 
 @task
 def task_3_3():
-    pass
+    toreno()
+
+    if wall_is_above() == True and wall_is_beneath() == True:
+            if wall_is_on_the_left() == True:
+                move_right(1)  
+            if wall_is_on_the_right() == True:
+                move_left(1) 
+
+    if wall_is_on_the_left() == True and wall_is_on_the_right() == True:
+            if wall_is_above() == True:
+                move_down(1)  
+            if wall_is_beneath() == True:
+                move_up(1) 
+
+def toreno():
+    print("toreno")                
+
 
 
 if __name__ == '__main__':
