519f87ca1219a43f3fba32236abb2f7825aee60a
miketoreno88 <toreno.m@mail.ru>
2022-12-22 21:51:15 +0300

    New solution task_4.py

--- task_4.py
+++ task_4.py
@@ -5,24 +5,18 @@ from pyrob.api import *
 
 @task
 def task_3_3():
-    toreno()
 
-    if wall_is_above() == True and wall_is_beneath() == True:
-            if wall_is_on_the_left() == True:
-                move_right(1)  
-            if wall_is_on_the_right() == True:
-                move_left(1) 
+    if wall_is_above() == False:
+        move_up()
+    
+    elif  wall_is_beneath() == False:
+        move_down()    
 
-    if wall_is_on_the_left() == True and wall_is_on_the_right() == True:
-            if wall_is_above() == True:
-                move_down(1)  
-            if wall_is_beneath() == True:
-                move_up(1) 
+    elif wall_is_on_the_left() == False:
+        move_left()
 
-def toreno():
-    print("toreno")                
-
-
+    elif wall_is_on_the_right() == False:
+        move_right()                    
 
 if __name__ == '__main__':
     run_tasks()
