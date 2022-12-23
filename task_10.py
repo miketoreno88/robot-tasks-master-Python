9bdc8f1e336af2ee854b9fdc6956702f8fb677d3
miketoreno88 <toreno.m@mail.ru>
2022-12-23 10:01:12 +0300

    modified:   task_10.py modified:   task_9.py

--- task_10.py
+++ task_10.py
@@ -5,7 +5,12 @@ from pyrob.api import *
 
 @task
 def task_8_3():
-    pass
+
+    while wall_is_above() == True or wall_is_beneath() == True:
+        fill_cell()
+        move_right()
+    while wall_is_above() != True:
+        move_right()        
 
 
 if __name__ == '__main__':
--- task_9.py
+++ task_9.py
@@ -5,13 +5,14 @@ from pyrob.api import *
 
 @task
 def task_8_2():
-    while wall_is_beneath() == True:
+    
+    while wall_is_on_the_right() == False:
         if wall_is_above() == False:
-            fill_cell()
-        if wall_is_on_the_right() == True:
-            break
-        move_right()
-  
+            fill_cell()  
+        move_right() 
+    if wall_is_above() == False:
+        fill_cell()
+           
 
 if __name__ == '__main__':
     run_tasks()
