6817bddb1671669a6140c615f792b7350f047339
miketoreno88 <toreno.m@mail.ru>
2023-01-08 21:31:26 +0300

    modified:   task_18.py

--- task_18.py
+++ task_18.py
@@ -5,8 +5,19 @@ from pyrob.api import *
 
 @task
 def task_8_28():
-    pass
-
+    direction = -1
+    while wall_is_above() == True:
+        if wall_is_on_the_left() == True: 
+            print('ky')
+            direction = 1
+        if direction == -1:
+            move_left()
+        elif direction == 1:
+            move_right()
+    while wall_is_above() == False:
+        move_up()
+    while wall_is_on_the_left() ==False:
+        move_left()           
 
 if __name__ == '__main__':
     run_tasks()
