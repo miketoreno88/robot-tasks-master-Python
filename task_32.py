86d9ac39293f6ce1f0f1122f8a5f01003b886d87
Toreno <miketorenocd@gmail.com>
2023-01-16 21:24:53 +0300

    add task 32

--- task_32.py
+++ task_32.py
@@ -5,8 +5,21 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_8_18():
-    pass
+    n = 0
+    while not wall_is_on_the_right():
+        if wall_is_above() and wall_is_beneath() is True:
+            fill_cell()
+        while not wall_is_above():
+            move_up()
+            if cell_is_filled() is True:
+                n = n + 1
+            else:
+                fill_cell()
+        while not wall_is_beneath():
+            move_down()
+        move_right()
 
+    mov("ax", n)
 
 if __name__ == '__main__':
     run_tasks()
