4c5284c36a2552b1b58abec54997e154e1f6a110
Toreno <miketorenocd@gmail.com>
2023-01-16 21:00:38 +0300

    add task 29, 30, 31

--- task_30.py
+++ task_30.py
@@ -4,40 +4,40 @@ import math
 from pyrob.api import *
 
 
-@task(delay=0.10)
+@task(delay=0.05)
 def task_9_3():
     i = 0
     while not wall_is_beneath():
+        move_down()
         i += 1
-        move_down()
-    j = math.floor(i/2)
-    print(j)
-    for k in range(j):
-        turnover(i)
-        turnover(i-2)
-    while not wall_is_on_the_left() and  not wall_is_beneath():
-        move_left()
-        move_down()
+        k = math.floor(i/2)
+    for n in range(k):
+        move('go_right', i-n*2)
+        move('go_up', i-n*2)
+        move('go_left', i-n*2)
+        move('go_down', i-n*2)
+        if n == k-1:
+            while not wall_is_on_the_left():
+                move_left()
+                move_down()
+        else:
+            move_up()
+            move_right()
 
-def turnover(i):
-    for j in range(i):
-        if j != 0 and j != i:
+
+def move(direction, i):        
+    for k in range(i):
+        if k != 0 and k != i:
             fill_cell()
-        move_right()
-    for j in range(i):
-        if j != 0 and j != i:
-            fill_cell()
-        move_up()
-    for j in range(i):
-        if j != 0 and j != i:
-            fill_cell()
-        move_left()
-    for j in range(i):
-        if j != 0 and j != i:
-            fill_cell()
-        move_down()
-    move_up()
-    move_right()
+        if direction == 'go_left':
+            move_left()
+        if direction == 'go_up':
+            move_up()
+        if direction == 'go_right':
+            move_right()
+        if direction == 'go_down':
+            move_down()    
+        
 
 
 if __name__ == '__main__':
--- task_31.py
+++ task_31.py
@@ -5,7 +5,25 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_8_30():
-    pass
+    while not wall_is_on_the_left() or not wall_is_beneath():
+        while not wall_is_beneath():
+            move_down()
+        while not wall_is_on_the_left() and wall_is_beneath():
+            move_left()
+            flag_left = True
+        while not wall_is_beneath():
+            move_down()
+            flag_left = False
+            flag_right = False
+        while not wall_is_on_the_right():
+            move_right()
+            if not wall_is_beneath():
+                move_down()
+            flag_right = True
+        if flag_right == True and flag_left == True:
+            while not wall_is_on_the_left():
+                move_left()
+            break
 
 
 if __name__ == '__main__':
